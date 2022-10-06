03a10025054564a33d90b9c7488905bb4b0d721d
FelixNievelstein <lixstein@gmx.de>
2022-10-06 12:35:20 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 var package = Package(
   name: "test-package"
   dependencies: [
-  .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 1),
+  .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 2),
         .Package(url: "https://github.com/PureSwift/BluetoothLinux.git", majorVersion: 3)]
 )
