0247fc6d07336dbbd4ca9c9ea9b6f6ea58fe739e
FelixNievelstein <lixstein@gmx.de>
2022-10-21 09:41:49 +0200

    Create Package@swift-5.1.swift

---
+++ Package@swift-5.1.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+var package = Package(
+  name: "test-package"
+  dependencies: [
+  .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 2),
+        .Package(url: "https://github.com/esemusa/test_spm_2.git")]
+)
