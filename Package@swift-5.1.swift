a3d70afe30903123a86567d1893573daa66f589e
FelixNievelstein <lixstein@gmx.de>
2022-10-21 10:10:52 +0200

    Update Package@swift-5.1.swift

--- Package@swift-5.1.swift
+++ Package@swift-5.1.swift
@@ -3,6 +3,6 @@ import PackageDescription
 var package = Package(
   name: "test-package"
   dependencies: [
-  .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 2),
+  .Package(url: "https://github.com/apple/example-package-dealer.git"),
         .Package(url: "https://github.com/esemusa/test_spm_2.git")]
 )
